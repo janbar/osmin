ecf01e2594be436abd8b62b4047bc4ebdda64886
janbar <jlbarriere68@gmail.com>
2024-09-13 22:07:46 +0200

    android: handle back key pressed on android
    - Use event onKeyDown for sdk <= 32 (up to android 12L)
    - Register callback 'on back invoked' for sdk >= 33 (android 13)

---
+++ android/package-sources/src/io/github/janbar/osmin/QtAndroidApplication.java
@@ -0,0 +1,11 @@
+package io.github.janbar.osmin;
+
+import org.qtproject.qt.android.QtApplicationBase;
+
+public class QtAndroidApplication extends QtApplicationBase
+{
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}
