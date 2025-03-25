cb75600d4312c0cc95b37e052073d3ff64af40ac
janbar <jlbarriere68@gmail.com>
2025-03-25 23:40:55 +0100

    android: set the find root path

--- android/linux-build-x86-qt515.sh
+++ android/linux-build-x86-qt515.sh
@@ -9,6 +9,7 @@ export QT_DIR=$HOME/bin/Qt/5.15.16/android
 
 cmake .. -B $BUILD_DIR -DCMAKE_SYSTEM_NAME=Android \
 -DCMAKE_PREFIX_PATH=$QT_DIR \
+-DCMAKE_FIND_ROOT_PATH=${QT_DIR} \
 -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake \
 -DCMAKE_MAKE_PROGRAM=$ANDROID_NDK/prebuilt/linux-x86_64/bin/make \
 -DCMAKE_BUILD_TYPE=Debug \
@@ -21,22 +22,6 @@ cmake .. -B $BUILD_DIR -DCMAKE_SYSTEM_NAME=Android \
 -DANDROID_SDK_ROOT=$ANDROID_SDK_ROOT \
 -DANDROID_NDK=$ANDROID_NDK \
 -DQT_ANDROID_PLATFORM_LEVEL=34 \
--DQt5_DIR=$QT_DIR/lib/cmake/Qt5 \
--DQt5Core_DIR=$QT_DIR/lib/cmake/Qt5Core \
--DQt5Gui_DIR=$QT_DIR/lib/cmake/Qt5Gui \
--DQt5Qml_DIR=$QT_DIR/lib/cmake/Qt5Qml \
--DQt5Network_DIR=$QT_DIR/lib/cmake/Qt5Network \
--DQt5Quick_DIR=$QT_DIR/lib/cmake/Qt5Quick \
--DQt5QuickControls2_DIR=$QT_DIR/lib/cmake/Qt5QuickControls2 \
--DQt5Xml_DIR=$QT_DIR/lib/cmake/Qt5Xml \
--DQt5Svg_DIR=$QT_DIR/lib/cmake/Qt5Svg \
--DQt5Widgets_DIR=$QT_DIR/lib/cmake/Qt5Widgets \
--DQt5Sensors_DIR=$QT_DIR/lib/cmake/Qt5Sensors \
--DQt5Positioning_DIR=$QT_DIR/lib/cmake/Qt5Positioning \
--DQt5Multimedia_DIR=$QT_DIR/lib/cmake/Qt5Multimedia \
--DQt5AndroidExtras_DIR=$QT_DIR/lib/cmake/Qt5AndroidExtras \
--DQt5QmlModels_DIR=$QT_DIR/lib/cmake/Qt5QmlModels \
--DQt5RemoteObjects_DIR=$QT_DIR/lib/cmake/Qt5RemoteObjects \
 $@
 
 [ $? -eq 0 ] && cmake --build $BUILD_DIR --parallel 8
