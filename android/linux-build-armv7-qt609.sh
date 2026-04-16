ac895d88664b46c63ed6053dc37fdcccd7b88d82
janbar <jlbarriere68@gmail.com>
2026-04-16 23:37:04 +0200

    build with qt framework 6.9.3

--- android/linux-build-armv7-qt609.sh
+++ android/linux-build-armv7-qt609.sh
@@ -2,11 +2,11 @@ BUILD_DIR=build-armv7-609
 rm -rf $BUILD_DIR/*
 mkdir -p $BUILD_DIR
 
-export JAVA_HOME=$HOME/bin/java/jdk-21.0.4
+export JAVA_HOME=$HOME/bin/java/jdk-21.0.10
 export ANDROID_SDK_ROOT=$HOME/bin/android/sdk
 export ANDROID_NDK=$HOME/bin/android/sdk/ndk/27.2.12479018
-export QT_DIR=$HOME/bin/Qt/6.9.2/android_armv7
-export QT_HOST_PATH=$HOME/bin/Qt/6.9.2/gcc_64
+export QT_DIR=$HOME/bin/Qt/6.9.3/android_armv7
+export QT_HOST_PATH=$HOME/bin/Qt/6.9.3/gcc_64
 
 cmake .. -B $BUILD_DIR -DCMAKE_SYSTEM_NAME=Android \
 -DCMAKE_PREFIX_PATH=${QT_DIR} \
